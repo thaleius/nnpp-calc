4c1b0973158391e531ad3d65d4b767af0c645102
thaleius <58767350+thaleius@users.noreply.github.com>
2026-07-07 01:10:42 +0200

    rewrite constants

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -4,13 +4,14 @@ const [C1, C2] = await getConstants();
 const T0 = 323;
 const T1 = 370;
 const T2 = 423;
+const P0 = 101.3;
 
 export function FR(T: number, FRV: number) {
-  return FRV/1000 * (pressure(T) - 101.3)**(1/2);
+  return FRV/1000 * (pressure(T) - P0)**(1/2);
 }
 
 export function FRV(T: number, FR: number) {
-  return T > 370 ? (1000*FR / (pressure(T) - 101.3)**(1/2)) : 0;
+  return T > T1 ? (1000*FR / (pressure(T) - P0)**(1/2)) : 0;
 }
 
 export function T(FRV: number, FR: number) {
@@ -18,7 +19,7 @@ export function T(FRV: number, FR: number) {
 }
 
 export function pressure(T: number) {
-  return (T > T1 ? T > T2 ? (C1 * (T-T0)) : (C1*C2 * (T-T1)**2) : 0) + 101.3;
+  return (T > T1 ? T > T2 ? (C1 * (T-T0)) : (C1*C2 * (T-T1)**2) : 0) + P0;
 }
 
 export function power(FR: number) {
