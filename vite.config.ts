a0fec5faa7ebd96f33078e7358c9bad95f6ca3ca
thaleius <58767350+thaleius@users.noreply.github.com>
2026-07-21 18:08:46 +0200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
@@ -8,26 +7,6 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		tailwindAutoReference(),
-		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter({
-				// default options are shown. On some platforms
-				// these options are set automatically — see below
-				pages: 'build',
-				assets: 'build',
-				fallback: 'index.html',
-				precompress: false,
-				strict: true
-			}),
-			prerender: {
-				entries: [] 
-			},
-			paths: {
-				base: process.argv.includes('dev') ? '' : '/nnpp-calc'
-			}
-		})
+		sveltekit()
 	]
 });
