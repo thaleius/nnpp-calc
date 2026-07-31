e00657bead7cba7b1f6bca09f22bb36dcb8dfa21
thaleius <58767350+thaleius@users.noreply.github.com>
2026-07-31 12:25:45 +0200

    add loading screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import './layout.css';
 	// import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
+  
+	onMount(() => {
+    const loader = document.getElementById('initial-loader');
+    if (loader) {
+      loader.style.opacity = '0';
+			loader.style.pointerEvents = 'none';
+			
+		setTimeout(() => {
+			loader.remove();
+		}, 500);
+    }
+  });
 </script>
 
 <svelte:head>
 	<!-- <link rel="icon" href={favicon} /> -->
 	<title>Calculator</title>
 </svelte:head>
+
 {@render children()}
 
 <style>
